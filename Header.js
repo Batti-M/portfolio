82870d1c162c28a82afcb35b7098a3136eb13f7f
Batti-M <116629327+Batti-M@users.noreply.github.com>
2022-11-01 11:15:17 +0100

    addet navbar-icon
    addet a navbar icon to toggle navigator

--- Header.js
+++ Header.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React,{useState,useContext, createContext} from 'react';
+import ScrollNavi from "./Scroll-Navi"
 
-const Header = () => {
+const Header = ({introductionref,aboutref,projectref}) => {
     return (
-      
-            <div className="header">
+        
+
+        <div className="header">
+                <NavbarIcon introductionref={introductionref} aboutref={aboutref} projectref={projectref}/>
                 <h1 className="name-short"> BM </h1>
                 <ul>
-                    <li className="list-item linkedIn"><a href="https://www.linkedin.com/in/bartosz-mieszkalski-30365916a/"><img className='logos' src="linkedin.png" alt="" /></a></li>   
-                    <li className="list-item git"><a href="https://github.com/batti-mies"><img className='logos' src="GitHub.png" alt="github-icon" /></a></li>   
+                       
+                    <li className="list-item git"><a href="https://github.com/Batti-M/portfolio"><img className='logos' src="GitHub.png" alt="github-icon" /></a></li>   
                  </ul>
-            </div>
+        </div>
+        
        
     )
 }
 
 export default Header
+
+
+
+const NavbarIcon = ({introductionref,aboutref,projectref}) => {
+    const[navActive,setNavActive] = useState("scroll-navigator")
+    
+    const toggleScrollNavi = () => {
+        navActive === "scroll-navigator" ? setNavActive("showNavi") : setNavActive("scroll-navigator")
+        navActive === "scroll-navigator" ? 
+        document.querySelector(".dropdown-icon").style.rotate = "135deg" : 
+        document.querySelector(".dropdown-icon").style.rotate = "-45deg"
+    }
+    return(
+        <>
+            <button onClick={toggleScrollNavi} className='dropdown-icon'></button>
+            <ScrollNavi introductionref={introductionref} aboutref={aboutref} projectref={projectref} navActive={navActive}/>
+        </>
+    
+    )
+
+}
